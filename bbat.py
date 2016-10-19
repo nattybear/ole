ad217c6c7a20576d3baf644de8a690478e443aee
Vincent <nattybear@naver.com>
2016-10-19 14:59:40 +0900

    add function that prints bbat entry chain

--- bbat.py
+++ bbat.py
@@ -1,4 +1,5 @@
-from ole_headerblock_info import fp, array_BBAT_depot_members
+from ole_headerblock_info import fp, array_BBAT_depot_members, startblock_property
+from struct import unpack
 
 BBAT = '' # Big Block Allocation Table
 
@@ -16,10 +17,40 @@ def array2table(fp, array, size):
         table += buf
     return table
     
+def read_entry(table, entry):
+    i = entry * 4
+    tmp = table[i:i+4]
+    entry = unpack('i', tmp)[0]
+    return entry
+    
+def table2chain(table, entry):
+    chain = []
+    while True:
+        if entry == -3:
+            print '[*] Special block'
+            return chain
+        elif entry == -2:
+            print '[*] End of chain'
+            return chain
+        elif entry == -1:
+            print '[*] Useless block'
+            return chain
+        else:
+            chain.append(entry)
+            entry = read_entry(table, entry)
+            
 BBAT = array2table(fp, array_BBAT_depot_members, 0x200)
+chain = table2chain(BBAT, startblock_property)
 
-fp2 = open('c:\\users\\user12\\ole\\BBAT.dump', 'wb')
-fp2.write(BBAT)
-fp2.close()
+if __name__ == '__main__':
+    fp2 = open('c:\\users\\user12\\ole\\BBAT.dump', 'wb')
+    fp2.write(BBAT)
+    fp2.close()
 
-print '[*] BBAT.dump saved'
+    print '[*] BBAT.dump saved'
+    
+    chain2 = []
+    for i in chain:
+        chain2.append('Entry ' + str(i))
+        
+    print ' -> '.join(chain2)
