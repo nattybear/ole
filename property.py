c3c0c6fabc9bf048d2fb130ab270aab506dfc244
Vincent <nattybear@naver.com>
2016-10-19 17:52:10 +0900

    now property.py can print property's name

--- property.py
+++ property.py
@@ -1,4 +1,5 @@
-from header import fp
+from struct import unpack
+from header import fp, bin2hex
 from bbat import chain, read_block
 
 def chain2storage(chain):
@@ -8,6 +9,33 @@ def chain2storage(chain):
         storage += buf
     return storage
     
+def single_property(fp, offset):
+    i = offset + 0x40
+    tmp = bin2hex(fp, i, 2)
+    size_pname = hex2short(tmp)
+    
+    i = offset
+    tmp = bin2hex(fp, i, size_pname)
+    pname = tmp.replace(b'\x00', '')
+    
+    if size_pname == 0:
+        return
+    
+    #print "Size of Property's name : ", size_pname
+    print "Name of Property : ", pname
+    
+    return
+    
+def hex2short(buf):
+    return unpack('H', buf)[0]
+    
+def block2property(fp, block_num):
+    i = block_num + 1
+    i2 = i * 0x200
+    for j in range(4):
+        j2 = i2 + (j * 0x80)
+        single_property(fp, j2)
+    
 property_storage = chain2storage(chain)
 
 if __name__ == '__main__':
@@ -16,3 +44,7 @@ if __name__ == '__main__':
     fp2.close()
     
     print '[*] property_storage.dump saved'
+    
+    for i in chain:
+        block2property(fp, i)
+    
