0ccb3feb2918e9baa0040aa6b25ac6c6a7407b86
Vincent <nattybear@naver.com>
2016-10-20 14:09:19 +0900

    change the way that make dump process. functionalize

--- sbat.py
+++ sbat.py
@@ -4,8 +4,8 @@ from header import startblock_SBAT, num_SBAT_depot, fp
 from bbat import table2chain, BBAT, array2table
 from property import property_list
 
-print '[*] Start block of SBAT : ', startblock_SBAT
-print '[*] Number of SBAT Depot : ', num_SBAT_depot
+#print '[*] Start block of SBAT : ', startblock_SBAT
+#print '[*] Number of SBAT Depot : ', num_SBAT_depot
 
 chain = table2chain(BBAT, startblock_SBAT)
 SBAT = array2table(fp, chain, 0x200)
@@ -16,6 +16,13 @@ root_chain = table2chain(BBAT, root_start)
 # Small Data Block
 SDB = array2table(fp, root_chain, 0x200)
 fSDB = StringIO(SDB)
+
+def pnum2data(SBAT, list, pnum, fSDB):
+    pnum2 = int(pnum)
+    pchain = table2chain(SBAT, property_list[pnum2]['start'])
+    pchain2 = [x - 1 for x in pchain]
+    pdata = array2table(fSDB, pchain2, 0x40)
+    return pdata
 
 if __name__ == '__main__':
     fp2 = open('c:\\users\\user12\\ole\\SBAT.dump', 'wb')
@@ -31,17 +38,15 @@ if __name__ == '__main__':
     print '[*] SDB.dump saved'
     
     try:
-        pnum = int(argv[1])
-        pchain = table2chain(SBAT, property_list[pnum]['start'])
-        pchain2 = [x - 1 for x in pchain]
-        pdata = array2table(fSDB, pchain2, 0x40)
+        pnum = argv[1]
+        pdata = pnum2data(SBAT, property_list, argv[1], fSDB)
         
-        fname = property_list[pnum]['name'] + '.dump'
+        fname = property_list[int(pnum)]['name'] + '.dump'
         fp4 = open(fname, 'wb')
         fp4.write(pdata)
         fp4.close()
         
         print '[*]', fname, 'saved'
-        
+    
     except:
         pass
