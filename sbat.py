3b98b098cfa54e2ccea92abed069901807754003
Vincent <nattybear@naver.com>
2016-10-20 11:35:56 +0900

    finally sbat can dump each property stream data

--- sbat.py
+++ sbat.py
@@ -1,4 +1,5 @@
 from sys import argv
+from StringIO import StringIO
 from header import startblock_SBAT, num_SBAT_depot, fp
 from bbat import table2chain, BBAT, array2table
 from property import property_list
@@ -14,6 +15,7 @@ root_chain = table2chain(BBAT, root_start)
 
 # Small Data Block
 SDB = array2table(fp, root_chain, 0x200)
+fSDB = StringIO(SDB)
 
 if __name__ == '__main__':
     fp2 = open('c:\\users\\user12\\ole\\SBAT.dump', 'wb')
@@ -31,6 +33,15 @@ if __name__ == '__main__':
     try:
         pnum = int(argv[1])
         pchain = table2chain(SBAT, property_list[pnum]['start'])
-        print pchain
+        pchain2 = [x - 1 for x in pchain]
+        pdata = array2table(fSDB, pchain2, 0x40)
+        
+        fname = property_list[pnum]['name'] + '.dump'
+        fp4 = open(fname, 'wb')
+        fp4.write(pdata)
+        fp4.close()
+        
+        print '[*]', fname, 'saved'
+        
     except:
         pass
